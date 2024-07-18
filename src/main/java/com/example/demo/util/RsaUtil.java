597569a83ed55477b31838ac9080d36a95f8c555
Admin <hai55jmt@gmail.com>
2024-07-18 11:36:58 +0700

    add

--- src/main/java/com/example/demo/util/RsaUtil.java
+++ src/main/java/com/example/demo/util/RsaUtil.java
@@ -1,11 +1,13 @@
 package com.example.demo.util;
 
+import com.example.demo.exception.IllegalArgumentMyFunctionException;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.lang.runtime.ObjectMethods;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -14,6 +16,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Base64;
+import java.util.Objects;
 
 @Component
 public class RsaUtil {
@@ -24,12 +27,18 @@ public class RsaUtil {
         return keyPairGenerator.generateKeyPair();
     }
     public String ecryptData(String data, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        if(Objects.isNull(data)||Objects.isNull(publicKey)){
+            throw new IllegalArgumentMyFunctionException();
+        }
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE,publicKey);
         byte[] encryptedData = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedData);
     }
     public String decryptData(String ecryptedText, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+        if(Objects.isNull(ecryptedText)||Objects.isNull(privateKey)){
+            throw new IllegalArgumentMyFunctionException();
+        }
         Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE,privateKey);
         byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(ecryptedText));
